6348f7c4157c44e5fefb65e39db56d7efcc4f99f
Hans Peter Hagblom <Hans.PeterHagblom@aerlingus.com>
2025-07-27 10:02:57 +0100

    Adding neovim lazygit

---
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,16 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      pylsp = {
+        settings = {
+          pylsp = {
+            plugins = {
+              pyflakes = { enabled = false },
+            },
+          },
+        },
+      },
+    },
+  },
+}
