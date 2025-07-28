f96467e2235ed3d42a6107ae995a56486de8c3c9
Hans Peter Hagblom <hagblom.hp@outlook.com>
2025-07-28 21:22:36 +0100

    Fixing some nvim settings

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,16 +1,30 @@
+#return {}
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      pylsp = {
-        settings = {
-          pylsp = {
-            plugins = {
-              pyflakes = { enabled = false },
-            },
-          },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          mason = false,
+          autostart = false,
         },
       },
     },
   },
 }
+--return {
+--  "neovim/nvim-lspconfig",
+--  opts = {
+--    servers = {
+--      pylsp = {
+--        settings = {
+--          pylsp = {
+--            plugins = {
+--              pyflakes = { enabled = false },
+--            },
+--          },
+--        },
+--      },
+--    },
+--  },
+--}
